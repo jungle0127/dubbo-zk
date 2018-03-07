b38f1ee31d9540e1c4d3be62f4650e45e1565045
jungle0127 <arbor@foxmail.com>
2018-03-07 20:53:26 +0800

    init project

---
+++ dubbo-api/src/main/java/org/dubbo/api/DemoService.java
@@ -0,0 +1,7 @@
+package org.dubbo.api;
+
+import java.util.List;
+
+public interface DemoService {
+	List<String> getPermissions(Long id);
+}
---
+++ dubbo-provider/src/main/java/org/dubbo/provider/App.java
@@ -0,0 +1,26 @@
+package org.dubbo.provider;
+
+import java.io.IOException;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("provider.xml");
+        System.out.println(context.getDisplayName() + ": here");
+        context.start();
+        System.out.println("服务已经启动...");
+        try {
+			System.in.read();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+}
